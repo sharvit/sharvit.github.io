05fdb86c0a4d6a4729326c8fe10d67c124b3abbb
Avi Sharvit <asharvit@redhat.com>
2019-08-10 10:45:56 +0300

    feat(html5): use html5 breadcrumbs

--- src/components/Layout/Breadcrumbs/BreadcrumbItem.js
+++ src/components/Layout/Breadcrumbs/BreadcrumbItem.js
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 
 const BreadcrumbItem = ({ title, path }) => (
-  <li>
-    <Link to={path}>{title}</Link>
+  <li itemScope itemType="http://data-vocabulary.org/Breadcrumb">
+    <Link to={path} itemProp="url">
+      <span itemProp="title">{title}</span>
+    </Link>
   </li>
 );
 
