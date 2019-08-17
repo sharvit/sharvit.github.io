5686d034e8ea923580fe1c3268a84ad5e73290f2
Avi Sharvit <sharvita@gmail.com>
2019-08-17 17:21:07 +0300

    feat(embed): adds embed options for md files

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,15 @@ module.exports = {
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        plugins: ['gatsby-remark-prismjs'],
+        plugins: [
+          {
+            resolve: '@raae/gatsby-remark-oembed',
+            options: {
+              usePrefix: ["oembed", "video"],
+            }
+          },
+          'gatsby-remark-prismjs',
+        ],
       },
     },
     {
