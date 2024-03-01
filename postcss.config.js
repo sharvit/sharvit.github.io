7ad9dfdb9194f977449879d200cd19c853b03dea
Avi Sharvit <sharvita@gmail.com>
2024-03-01 12:24:07 +0700

    ref: upgrade postcss-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,17 @@
 const postcssImport = require('postcss-import');
-const postcssCustomSelectors = require('postcss-custom-selectors');
 const postcssPresetEnv = require('postcss-preset-env');
-const combineDuplicatedSelectors = require('postcss-combine-duplicated-selectors');
 
 module.exports = () => ({
   plugins: [
-    postcssImport(),
-    postcssCustomSelectors({
-      importFrom: ['src/theme/styles/theme.selectors.css'],
+    postcssImport({
+      path: ['src'],
     }),
     postcssPresetEnv({
       stage: 2,
       features: {
         'nesting-rules': true,
-        'custom-media-queries': {
-          importFrom: ['node_modules/open-props/media.min.css'],
-        },
+        'custom-selectors': true,
       },
     }),
-    combineDuplicatedSelectors(),
   ],
 });
